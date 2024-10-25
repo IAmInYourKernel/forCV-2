ee6006fa03ef425d79ab6baf790c351c23d1c0c5
Soumyajit Bhattacharya <106494351+IAmInYourKernel@users.noreply.github.com>
2024-10-26 02:03:09 +0530

    Update serverData.dart
    removed all WS and API END POINT

--- healthka_customers/lib/datahandler/serverData.dart
+++ healthka_customers/lib/datahandler/serverData.dart
@@ -1,10 +1,10 @@
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-String localAPI = "http://192.168.4.58:3000/";
-String remoteAPI = "http://3.94.234.149:3000/";
+String localAPI = "";
+String remoteAPI = "";
 String serverAPI = localAPI;
 
-String localRideRequestWS = "ws://192.168.0.102:3001/";
-String localOngoingRideWS = "ws://192.168.0.102:3002/";
+String localRideRequestWS = "";
+String localOngoingRideWS = "";
 WebSocketChannel? rideRequestChannel;
 WebSocketChannel? ongoingRideChannel;
